f36d82d9032c691dcae54ff7181d3e919481fce2
alikiminza <ali.kiminza@student.moringaschool.com>
2023-06-21 08:24:14 +0300

    final record

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const superbowlWin = (record) => {
+    let Win =  record.find((record) => record.result === "W");
+
+
+    if (Win){
+        return Win.year
+    }
+
+}
+console.log(superbowlWin(record))
